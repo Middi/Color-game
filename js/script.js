a5e92ae9d44f3d1c3f56b38226f9be1575a24582
“Richard Middleton” <Richard@richardmiddleton.me>
2017-07-06 22:15:31 +0200

    generated random colors

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-    ];
+var colors = generateRandomColors(6);
+
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
 
@@ -21,6 +15,7 @@ for(var i = 0; i < squares.length; i++) {
 
        if(clickedColor === pickedColor){
            messageDisplay.textContent = "Correct";
+           changeColors(clickedColor);
        }
        else {
            this.style.backgroundColor = "#232323";
@@ -29,3 +24,29 @@ for(var i = 0; i < squares.length; i++) {
     });
 }
 
+function changeColors(color) {
+    for(var i = 0; i < colors.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor(){
+    var random =Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateRandomColors(num) {
+    var arr = [];
+    for(var i = 0; i < num; i++){
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor(){
+   var r = Math.floor(Math.random() * 256);
+   var g = Math.floor(Math.random() * 256);
+   var b = Math.floor(Math.random() * 256);
+   
+   return "rgb(" + r + ", " + g + ", " + b + ")";
+}
